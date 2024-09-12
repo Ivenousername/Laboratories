ed4c8420bb1ce40e91934df869d7ebbe639bf5df
Ivenousername <anondaun42@gmail.com>
2024-09-12 11:43:12 +0700

    Update Matrices.cpp, Matrices.h, OOP_1.cpp, and 2 more files

---
+++ OOP_1/Matrices.h
@@ -0,0 +1,7 @@
+#pragma once
+
+int** CreateMatrix(int n);
+
+void FillMatrixRandom(int** matrix, int n);
+
+void PrintMatrix(int** matrix, int n);
--- OOP_1/OOP_1.cpp
+++ OOP_1/OOP_1.cpp
@@ -1,20 +1,37 @@
-// OOP_1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include "Matrices.h"
+int* NextPerm(int* current_perm) {
+	return current_perm;
+}
+int BruteForce(int** matrix, int n_cities, int starting_city) {
+	int min_value = INT_MAX;
+	int* current_perm = new int[n_cities];
+	current_perm[0] = 0;
+	int n_perm = 1;
+	for (int i = 1; i < n_cities; i++) {
+		current_perm[i] = i;
+		n_perm *= i;
 
+	} //current perm is like [0 1 2 3 4] 
+	int current_weight = 0;
+	for (int i = 0; i < n_perm-1; i++) { //Counting the weight of the current permutation
+		for(int j=0;j<n_cities)
+		current_weight += matrix[current_perm[i]][current_perm[i + 1]];
+	}
+}
 int main()
 {
-    std::cout << "Hello World!\n";
+	std::cout << "Matrix size:  ";
+	int n;
+	std::cin >> n;
+	std::cout << "\nStarting city:  ";
+	int starting_city;
+	std::cin >> starting_city;
+	
+	int** matrix = CreateMatrix(4);
+	FillMatrixRandom(matrix, 4);
+	PrintMatrix(matrix, 4);
+
+	std::cout << '\n' << BruteForce(matrix,n,starting_city);
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
